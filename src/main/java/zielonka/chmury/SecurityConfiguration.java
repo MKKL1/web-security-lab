3d3b7169cff4e84b5ba57b34afb631efbea7f29f
MKKL <Mkkl3301@gmail.com>
2025-10-20 17:09:02 +0200

    disable csrf protection temporarily

--- src/main/java/zielonka/chmury/SecurityConfiguration.java
+++ src/main/java/zielonka/chmury/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package zielonka.chmury;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +11,8 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
